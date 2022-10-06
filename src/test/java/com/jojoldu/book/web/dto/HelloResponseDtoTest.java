48aefcffd2dafdbe10ecd54784ee54cd5b0c9796
inno8 <innovale@kakao.com>
2022-10-07 01:48:12 +0900

    2장까지

---
+++ src/test/java/com/jojoldu/book/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.jojoldu.book.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복기능테스트() {
+        //junit은 다른 거에 의존적이다
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
